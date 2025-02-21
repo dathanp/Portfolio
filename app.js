0d58580b803febcd2289b34fa08b719a513ce8c2
Dathan Pompa <dathanpompa@gmail.com>
2025-02-21 18:46:58 +0900

    Created New Page
    Added new page to talk about my enhancements, added mobile friendly features.

--- app.js
+++ app.js
@@ -49,4 +49,11 @@ cards.forEach(card => {
   });
 });
 
-  
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.querySelector(".menu-toggle");
+  const navLinks = document.querySelector(".nav-links");
+
+  menuToggle.addEventListener("click", function () {
+    navLinks.classList.toggle("nav-active");
+  });
+});
